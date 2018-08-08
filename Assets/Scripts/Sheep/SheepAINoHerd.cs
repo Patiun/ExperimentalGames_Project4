19c48ffb6f047ef8eba49d9d1705df8e53fb2232
Greg Kilmer <remlik.mk@gmail.com>
2018-08-08 02:26:15 -0400

    Updated flocking to make sheep flee from things tagged as scary

--- Assets/Scripts/Sheep/SheepAINoHerd.cs
+++ Assets/Scripts/Sheep/SheepAINoHerd.cs
@@ -6,22 +6,29 @@ public class SheepAINoHerd : MonoBehaviour {
 
     public List<GameObject> neighbors;
     public float speed;
-    public float alignmentWeight, cohesionWeight, seperationWeight;
+    public float alignmentWeight, cohesionWeight, seperationWeight, fearWeight;
     public float seperationVariance;
     public float tooCloseDistance = 1.25f;
+    public float rotationSpeed = 1f;
+    public bool scared;
+    public List<GameObject> scaryThings;
 
     private Rigidbody rb;
+    private float originalCohesion, originalSeperation, originalSpeed;
 
 	// Use this for initialization
 	void Start () {
         neighbors = new List<GameObject>();
         rb = GetComponent<Rigidbody>();
+        originalCohesion = cohesionWeight;
+        originalSeperation = seperationWeight;
+        originalSpeed = speed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        rb.velocity = Vector3.Lerp(rb.velocity,(ComputeAlignment() * alignmentWeight + ComputeCohesion() * cohesionWeight + ComputeSeperation() * seperationWeight).normalized * speed,0.35f);
-        //transform.LookAt(ComputeAlignment()+transform.position);
+        rb.velocity = Vector3.Lerp(rb.velocity,(ComputeAlignment() * alignmentWeight + ComputeCohesion() * cohesionWeight + ComputeSeperation() * seperationWeight + ComputeFear() * fearWeight).normalized * speed,0.35f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(rb.velocity), Time.deltaTime * rotationSpeed);
 	}
 
     public void OnTriggerEnter(Collider other)
@@ -34,6 +41,18 @@ public class SheepAINoHerd : MonoBehaviour {
                 neighbors.Add(other.gameObject);
             }
         }
+
+        if (other.tag == "Scary")
+        {
+            scared = true;
+            cohesionWeight *= 2f;
+            seperationVariance *= 0.8f;
+            speed *= 1.5f;
+            if (!scaryThings.Contains(other.gameObject))
+            {
+                scaryThings.Add(other.gameObject);
+            }
+        }
     }
 
     public void OnTriggerExit(Collider other)
@@ -44,6 +63,18 @@ public class SheepAINoHerd : MonoBehaviour {
             if (neighbors.Contains(other.gameObject))
             {
                 neighbors.Remove(other.gameObject);
+            }
+        }
+
+        if (other.tag == "Scary")
+        {
+            scared = false;
+            cohesionWeight = originalCohesion;
+            seperationVariance = originalSeperation;
+            speed = originalSpeed;
+            if (scaryThings.Contains(other.gameObject))
+            {
+                scaryThings.Remove(other.gameObject);
             }
         }
     }
@@ -91,6 +122,22 @@ public class SheepAINoHerd : MonoBehaviour {
         dir = dir / neighbors.Count;
 
         return dir;
+    }
+
+    //TODO Gets stuck in corners because rear of flock aren't scared but leaders are
+    public Vector3 ComputeFear()
+    {
+        Vector3 dir = Vector3.zero;
+        if (scared)
+        {
+            foreach (GameObject thing in scaryThings)
+            {
+                Vector3 dif = thing.transform.position - transform.position;
+                dir -= dif;
+            }
+        }
+        dir = dir / scaryThings.Count;
+        return dir.normalized;
     }
 
     public Vector3 GetVelocity()
